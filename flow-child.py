8c5187285982a413aa7b097e556f9a35c82401df
Your Name <you@example.com>
2025-08-06 09:15:38 +0000

    7th cpommit

--- flow-child.py
+++ flow-child.py
@@ -6,17 +6,23 @@ from prefect.events import DeploymentEventTrigger
 def tata():
     print("TATA")
 
+
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/moulin-louis/tmp-prefect.git",
-        entrypoint="flow-child.py:tata"
+        entrypoint="flow-child.py:tata",
     ).deploy(
         name="child-deployement",
         work_pool_name="test-work-pool",
         triggers=[
             DeploymentEventTrigger(
+                name="toto-succeeded-trigger",
                 expect={"prefect.flow-run.Completed"},
-                match_related={"prefect.resource.name": "parent-deployement"},
+                match={"prefect.resource.id": "prefect.flow-run.*"},
+                match_related={
+                    "prefect.resource.name": "parent-deployement",
+                    "prefect.resource.role": "flow",
+                },
             )
         ],
     )
