f43f7bfd574edc9027e050764b468da9010a0727
Your Name <you@example.com>
2025-08-06 08:47:36 +0000

    2nd commit

--- flow-parent.py
+++ flow-parent.py
@@ -4,9 +4,14 @@ from prefect import flow
 @flow
 def toto():
     print("Hello")
+    with open("/tmp/log", "a") as f:
+        f.write("FDSDSFDS")
 
 if __name__ == "__main__":
-    toto.deploy(
+    flow.from_source(
+        source="https://github.com/moulin-louis/tmp-prefect.git",
+        entrypoint="flow-parent.py:toto"
+    ).deploy(
         name="parent-flow",
         work_pool_name="test-work-pool",
         cron="*/5 * * * *",
